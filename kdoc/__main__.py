acc4c0b5ca0c7ef82540f4a0cf893d2e65f9d557
jdiez17 <jose.manuel.diez@gmail.com>
2013-09-21 01:13:58 +0200

    Added requirements, license, main

---
+++ kdoc/__main__.py
@@ -0,0 +1,64 @@
+from .processor import Processor
+
+import sys
+import pprint
+import os
+import re
+
+from jinja2 import Template, Environment, PackageLoader, Markup
+from markdown import markdown
+
+p = Processor()
+link_re = re.compile('\[\[\w+\]\]')
+
+def safe_markdown(text):
+    return Markup(markdown(text))
+
+def safe_links(text):
+    new_text = text
+
+    for match in link_re.findall(text):
+        func_name = str(match.strip('[]'))
+        if func_name not in p.lookup_table:
+            new_text = new_text.replace(match, Markup('<span class="deadlink">%s</span>' % func_name))
+            continue
+
+        category = p.lookup_table[func_name]
+        print category, func_name
+        new_text = new_text.replace(match, Markup('<a href="%s.html#%s">%s</a>' % (category, func_name, func_name)))
+
+    return new_text
+
+env = Environment(loader=PackageLoader("kdoc", "templates"))
+env.filters['markdown'] = safe_markdown
+env.filters['links'] = safe_links
+
+if __name__ == '__main__':
+    for f in sys.argv[1:]:
+        p.add(f)
+
+    p.process()
+
+    f_count = len(p.functions)
+    if f_count:
+        pp = pprint.PrettyPrinter(indent=4)
+        print "There are %d functions." % f_count
+        print "---"
+    else:
+        sys.exit(0)
+
+    template = env.get_template("category.html") 
+
+    try:
+        os.mkdir("html")
+    except: pass
+
+    for category, items in p.category_table.items():
+        content = template.render(name=category, items=items)
+
+        filename = "%s.html" % category.lower().replace(' ', '_')
+        with open(os.path.join("html", filename), "w") as f:
+            f.write(content)
+            f.flush()
+
+        print category, "written"
